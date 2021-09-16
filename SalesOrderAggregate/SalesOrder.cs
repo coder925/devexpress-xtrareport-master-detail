8a23132b958dbbd3fa3eb42eca11e98b6596767b
Michael Bergman <michael@simutek.se>
2021-09-16 14:36:24 +0200

    List of sales order.

--- SalesOrderAggregate/SalesOrder.cs
+++ SalesOrderAggregate/SalesOrder.cs
@@ -2,34 +2,37 @@
 
 public class SalesOrder
 {
-    public static readonly SalesOrder SampleData = new SalesOrder
-    {
-        Number = 200101,
-        Name = "Fashion week campaign",
-        SalesOrderLines = new List<SalesOrderline>
-            {
-                new SalesOrderline { Description = "Coffee mug", Quantity = 5, Price = 200 },
-                new SalesOrderline { Description = "T-shirt", Quantity = 15, Price = 107.50m },
-                new SalesOrderline
-                {
-                    Description = "Businss card",
-                    Quantity = 200,
-                    Price = 5000,
-                    Product = new Product()
-                    {
-                        Name = "Business card",
-                        ProductSpecifications = new List<ProductSpecificationRow>()
-                        {
-                            new ProductSpecificationRow() { Column1 = "Run:", Column2 = "", Column3 = "1000 copies"},
-                            new ProductSpecificationRow() { Column1 = "Format:", Column2 = "", Column3 = "A5"},
-                            new ProductSpecificationRow() { Column1 = "Pages:", Column2 = "", Column3 = "12 pages"},
-                        }
-                    }
-                }
-            }
-    };
+	public static readonly List<SalesOrder> SampleData = new List<SalesOrder>()
+	{
+		new SalesOrder
+		{
+			Number = 200101,
+			Name = "Fashion week campaign",
+			SalesOrderLines = new List<SalesOrderline>
+			{
+				new SalesOrderline { Description = "Coffee mug", Quantity = 5, Price = 200 },
+				new SalesOrderline { Description = "T-shirt", Quantity = 15, Price = 107.50m },
+				new SalesOrderline
+				{
+					Description = "Businss card",
+					Quantity = 200,
+					Price = 5000,
+					Product = new Product()
+					{
+						Name = "Business card",
+						ProductSpecifications = new List<ProductSpecificationRow>()
+						{
+							new ProductSpecificationRow() { Column1 = "Run:", Column2 = "", Column3 = "1000 copies"},
+							new ProductSpecificationRow() { Column1 = "Format:", Column2 = "", Column3 = "A5"},
+							new ProductSpecificationRow() { Column1 = "Pages:", Column2 = "", Column3 = "12 pages"},
+						}
+					}
+				}
+			}
+		}
+	};
 
-    public int Number { get; set; }
+	public int Number { get; set; }
 
 	public string Name { get; set; }
 
